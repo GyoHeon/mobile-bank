9c158dcf0eaa2c5415f65051e56a65c2b4c8329f
GyoHeon Lee <ghl12078@gmail.com>
2021-12-20 04:38:46 +0900

    feat: add json read-test

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 //import fetch from "../node_modules/node-fetch";
 //import 로 바꿔서 실행할 예정
-const data = require("../data/bank.json");
 
-const history = document.querySelector(".contents__history--ul__outer");
-const li = document.createElement("li");
+const history = document.querySelector(".contents__history--ul__inner");
 
-history.append("test");
-
-console.log(data["bankList"][0]);
-//
+fetch("https://eulsoo.github.io/generated_1.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (myJson) {
+    myJson.forEach((x) => {
+      const li = document.createElement("li");
+      const content = document.createTextNode(`${x.date} ${x.item} ${x.price}`);
+      li.appendChild(content);
+      history.appendChild(li);
+    });
+  });
